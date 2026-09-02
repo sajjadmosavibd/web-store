a29089107b8de47472247780cae50833d6e42f8e
Sajjad <sajjadmosavibdone@gmail.com>
2026-09-02 20:40:40 +0330

    randome static line handeled

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+
+const backgroundContainer=document.querySelector('#background') ;
+const lineCount=100;
+
+const setRandomPosition=(tag)=>{
+tag.style.top=`${Math.random()*100}%`;
+tag.style.left=`${Math.random()*100}%`;
+}
+
+const setLines=()=>{
+for(let i=0 ; i < lineCount ; i++){
+    const line=document.createElement('div');
+    line.classList.add('line');
+    setRandomPosition(line);
+    backgroundContainer.appendChild(line)
+}
+}
+
+window.addEventListener('load',setLines);
